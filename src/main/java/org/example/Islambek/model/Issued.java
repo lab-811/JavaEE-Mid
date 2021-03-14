b2b77a4b132d01983bf3043d41706b3037c791d6
Исламбек Нурмуханов <islambeknurmuhanov@MacBook-Pro-Islambek.local>
2021-03-14 21:58:05 +0500

    final-version

---
+++ src/main/java/org/example/Islambek/model/Issued.java
@@ -0,0 +1,26 @@
+package org.example.Islambek.model;
+
+public class Issued {
+    private Long id;
+    private Long book_id;
+
+    public Issued() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(Long book_id) {
+        this.book_id = book_id;
+    }
+}
